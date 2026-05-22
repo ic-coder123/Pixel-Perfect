55b3180653af0f3022d426064f6d7db595b5f41a
ic-coder123 <eishancc@gmail.com>
2026-05-22 13:47:27 -0400

    Fixed enemy tweaking out bug

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -2,13 +2,8 @@ extends CharacterBody2D
 @onready var hitbox: Area2D = $Hitbox
 @onready var edge_detector: RayCast2D = $edge_detector
 const SPEED = 100.0
-const DETECTION_RADIUS = 200.0
 var direction := -1
 var health := 50
-
-
-
-var is_chasing: bool = false
 
 func _ready() -> void:
 	add_to_group("enemy")
@@ -23,27 +18,12 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += 980.0 * delta
 
-	# Player detection
-	var player = get_tree().get_first_node_in_group("player")
-	if is_instance_valid(player):
-		var distance_to_player = global_position.distance_to(player.global_position)
-		is_chasing = distance_to_player < DETECTION_RADIUS
-	else:
-		is_chasing = false
-		
-	if is_chasing:
-		# Chase logic: move towards the player
-		var direction_to_player = sign((player.global_position - global_position).x)
-		if direction_to_player:
-			direction = direction_to_player
-			scale.x = abs(scale.x) * direction
-	else:
-		# Patrol logic: turn at edges
-		if is_on_floor() and not edge_detector.is_colliding():
-			print("Turning around at edge")
-			direction *= -1
+	# Patrol logic: turn at edges
+	if is_on_floor() and not edge_detector.is_colliding():
+		print("Turning around at edge")
+		direction *= -1
+		edge_detector.target_position.x = abs(edge_detector.target_position.x) * direction
 
-			edge_detector.target_position.x = abs(edge_detector.target_position.x) * direction
 		
 	# Move in current direction
 	velocity.x = direction * SPEED
